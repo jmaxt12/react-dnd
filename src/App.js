fe5599743c650b62788fc0ed85a267fb992a3a49
Jake Thomas <jmaxt12@gmail.com>
2020-01-22 12:52:01 -0700

    packages up to date with showing races

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+
+import Dnd from './container/dnd/dnd'
 import './App.css';
 
 class App extends Component {
+  
   render() {
     return (
       <div className="App">
         <p>STARTING POINT</p>
+        <Dnd />
       </div>
     );
   }
