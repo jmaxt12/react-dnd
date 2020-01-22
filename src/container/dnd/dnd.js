5e83d40a62e31addc98df470a8c4f3caa0031ae6
Jake Thomas <jmaxt12@gmail.com>
2020-01-22 13:37:53 -0700

    added login to show dnd class component

--- src/container/dnd/dnd.js
+++ src/container/dnd/dnd.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Race from '../../components/Characters/Race/Race';
+import Class from '../../components/Characters/Class/Class';
 
 class Dnd extends Component {
     state = {
-        races: []
+        races: [],
+        classes: []
     }
     
     componentDidMount () {
         axios.get( "http://www.dnd5eapi.co/api/races" )
             .then(response => {
                 this.setState({races: response.data.results})
-                console.log(response.data.results)
+                console.log("races", response.data.results)
+            })
+        axios.get("http://www.dnd5eapi.co/api/classes")
+            .then( response => {
+                this.setState({classes: response.data.results})
+                console.log("classes", response.data.results)
             })
     }
 
@@ -23,7 +30,15 @@ class Dnd extends Component {
                 key={race.index} 
                 name={race.name} 
                 />;
-        });
+            });
+
+       const classes = this.state.classes.map(clas => {
+           return <Class 
+            key={clas.index}
+            name={clas.name}
+            />
+       })
+                        
         return(
             <div>
                 <section>
@@ -32,7 +47,7 @@ class Dnd extends Component {
                 </section>
                 <section>
                     <h1>Classes</h1>
-                    {/* {classes} */}
+                    {classes}
                 </section>
             </div>
         )
